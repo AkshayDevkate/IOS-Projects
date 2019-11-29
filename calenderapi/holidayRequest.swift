8142d937bd4c97a531fc1c3207c545dc59fca024
Akshay Dattatray Devkate <42045980+AkshayDevkate@users.noreply.github.com>
2019-11-29 23:35:51 +0530

    Create holidayRequest.swift

---
+++ calenderapi/holidayRequest.swift
@@ -0,0 +1,15 @@
+//
+//  HolidayRequest.swift
+//  facebookapi
+//
+//  Created by Akshay Devkate on 29/11/19.
+//  Copyright © 2019 Akshay Devkate. All rights reserved.
+//
+
+import Foundation
+
+enum HolidayError: Error{
+    case NoDataAvailable
+    case canNotProcessData
+    
+}
