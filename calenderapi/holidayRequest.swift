fdc74b65f0a3f09012b19c8562790103315ae3a4
Akshay Dattatray Devkate <42045980+AkshayDevkate@users.noreply.github.com>
2019-11-29 23:36:05 +0530

    Update holidayRequest.swift

--- calenderapi/holidayRequest.swift
+++ calenderapi/holidayRequest.swift
@@ -13,3 +13,32 @@ enum HolidayError: Error{
     case canNotProcessData
     
 }
+
+
+struct HolidayRequest {
+    let resourseURL :URL
+    let API_KEY = "2532646de05df3ba30eabd92d8e1a884e746094a"
+    
+    init(CountryCode:String){
+        
+        let date = Date()
+        let format = DateFormatter()
+        format.dateFormat = "yyyy"
+        let currentYear = format.string(from: date)
+        
+        
+        let resourceString = "https://calendarific.com/api/v2/holidays?\(API_KEY)&country=\(CountryCode)&year=\(currentYear)"
+        guard let resourseURL = URL(string: resourceString)else{fatalError()}
+        self.resourseURL = resourseURL
+    }
+    func getHolidays (completion: @escaping(Result<[HolidayDetails],HolidayError>)-> Void){
+        let dataTask = URLSession.shared.dataTask(with: resourseURL){ data, _, _ in
+            guard let jsonData = data else{
+                completion(.failure(.NoDataAvailable))
+                return
+            }
+        }
+        dataTask.resume()
+    }
+    
+}
